2e9c5e1964b7843f023928374eb5ff3221ba5cfa
jRichard-Prado <jrichip@hotmail.com>
2025-03-11 17:47:04 +0100

    navegacion de vuelta

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-proyectos',
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './proyectos.component.html',
   styleUrl: './proyectos.component.css'
 })
