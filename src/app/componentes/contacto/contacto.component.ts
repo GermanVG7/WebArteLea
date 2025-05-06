37552eb16222796dd76c1e9a796758460ab39941
jRichard-Prado <jrichip@hotmail.com>
2025-05-06 15:42:15 +0200

    menu upgrade

--- src/app/componentes/contacto/contacto.component.ts
+++ src/app/componentes/contacto/contacto.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { MenuPrincipalComponent } from '../menu-principal/menu-principal.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-contacto',
-  imports: [MenuPrincipalComponent],
+  imports: [MenuPrincipalComponent,RouterLink,RouterLinkActive,RouterOutlet],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
 
+
+  
 }
